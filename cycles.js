f011b6067757303544c4eb15aa7fecc67be56d21
bdsummer2018 <example@i.ua>
2018-08-27 19:16:35 +0300

    comment

---
+++ cycles.js
@@ -0,0 +1,7 @@
+var i=1;
+var sum=0;
+while(i<101) {
+    sum=sum+i;
+    i++;
+}
+console.log(sum)
---
+++ switch.js
@@ -0,0 +1,16 @@
+
+var a = 5;
+
+switch (a) {
+    case 2:
+        console.log("hello");
+        break;
+    case 5:
+        console.log("world");
+        break;
+    default:
+        console.log("!!!");
+}
+
+
+
